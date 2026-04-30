b785972565800c895847c9a0f64e64aec2d59fe9
Sl33pySage <52090741+Sl33pySage@users.noreply.github.com>
2026-04-30 15:30:25 -0500

    Starting from absolute scratch

--- index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const hello = (name) => {
-    console.log(`Hello ${name}!`);
-};
-hello("TypeScript");
-//# sourceMappingURL=index.js.map
