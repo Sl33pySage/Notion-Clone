b9d244d39786771d98f26aff658b8d9f14d72a3f
Sl33pySage <52090741+Sl33pySage@users.noreply.github.com>
2026-05-02 02:35:59 -0500

    On Spacer Component

---
+++ Notion-Clone/src/Page/Cover.tsx
@@ -0,0 +1,26 @@
+import styles from "./Cover.module.css";
+import { useRef, type ChangeEventHandler } from "react";
+export const Cover = () => {
+  const fileInputRef = useRef<HTMLInputElement>(null);
+  const onChangeCoverImage = () => {
+    fileInputRef.current?.click();
+  };
+  const onCoverImageUpload: ChangeEventHandler<HTMLInputElement> = (event) => {
+    const target = event.target;
+    console.log(target?.files?.[0]);
+  };
+  return (
+    <div className={styles.cover}>
+      <img className={styles.image} src="" alt="Cover" />
+      <button onClick={onChangeCoverImage} className="styles.button">
+        Change cover
+      </button>
+      <input
+        onChange={onCoverImageUpload}
+        style={{ display: "none" }}
+        ref={fileInputRef}
+        type="file"
+      />
+    </div>
+  );
+};
