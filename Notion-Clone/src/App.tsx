021231ed9bc5e1c088b4fd74e148205ff797e9f9
Sl33pySage <52090741+Sl33pySage@users.noreply.github.com>
2026-05-03 16:10:10 -0500

    On the next Chapter: Notion Clone Move State Management to Context

--- Notion-Clone/src/App.tsx
+++ Notion-Clone/src/App.tsx
@@ -1,10 +1,11 @@
 import "./App.css";
 import { Cover } from "./Page/Cover.tsx";
+import { Page } from "./Page/Page";
 
 function App() {
   return (
     <>
-      <Cover />
+      <Page />
     </>
   );
 }
